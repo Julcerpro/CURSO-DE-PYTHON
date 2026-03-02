8b124b8fd984aca936a1adc6efb42269056199b1
JCPB2000 <jcpaguay1@espe.edu.ec>
2026-03-02 13:28:21 -0500

    actualizacion

---
+++ tipos_de_datos.py
@@ -0,0 +1,65 @@
+TIPO DE DATO
+
+'ejemplo'
+
+"string"
+
+"""string"""
+
+'''string'''
+
+"""ejemplo:
+    nombre: J
+    apellrito: P
+"""
+int
+40
+
+float
+42.5
+
+
+bool
+True False
+
+
+VARIABLES
+ESPACION QUE SE ALMACENA EN LA MEMORIA
+
+
+nombre = "J"
+bienvenida = "HOLA" + nombre + "COMO ESTAS"
+
+print(bienvenida)
+
+
+
+SE AUMENTA EL f AL INICAR EL VALOR DE LA VARIABLE PARA TANSFORMAR A TEXTO
+nombre = "J"
+bienvenida = f"HOLA" + {nombre} + "COMO ESTAS"
+
+print(bienvenida)
+
+OPERADOR
+del   BORRA EN ESTE CASO LA VARIABLE
+
+nombre = "J"
+bienvenida = "HOLA" + nombre + "COMO ESTAS"
+del bienvenida
+print(bienvenida)
+
+
+OPERADOR DE PERTENECIA
+
+in 
+not in   Con estas 2 formas consultamos si esos datos existen en la variable y 
+considerar que es sensible a mayusculas y minisculas si en caso esta mostrara True o False 
+
+nombre = "J"
+bienvenida = "HOLA" + nombre + "COMO ESTAS"
+
+print( "Hola" in bienvenida)
+
+
+
+
